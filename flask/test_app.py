ce7140430902ae8fa32aa3e9b2430d6c037215c2
CamilaZMS <sista.batera@gmail.com>
2024-12-01 09:10:17 -0300

    cria teste cadastro de aluno

---
+++ flask/test_app.py
@@ -0,0 +1,33 @@
+import os
+import unittest
+from app import app, db, Aluno
+
+class TestApp(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Definir o ambiente como 'testing'
+        os.environ['FLASK_ENV'] = 'testing'
+
+    def setUp(self):
+        # Configurar o banco de dados em memória
+        with app.app_context():
+            db.create_all()
+
+    def tearDown(self):
+        # Limpar o banco de dados após cada teste
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+
+    def test_cadastrar_aluno(self):
+        with app.app_context():
+            aluno = Aluno(nome="Camila", ra="04829")
+            db.session.add(aluno)
+            db.session.commit()
+
+            aluno_salvo = Aluno.query.filter_by(ra="04829").first()
+            self.assertIsNotNone(aluno_salvo)
+            self.assertEqual(aluno_salvo.nome, "Camila")
+
+if __name__ == '__main__':
+    unittest.main()
